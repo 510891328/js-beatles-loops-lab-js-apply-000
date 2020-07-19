73f1e030df252daf2acb637a992b7727fbd91460
Yunhao Gao <as510891328@gmail.com>
2020-07-19 19:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(var i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+  var count = 0
+  while(facts !== null){
+    facts[count] = facts[count] + '!!!'
+    count++
+  }
+  return facts
+}
